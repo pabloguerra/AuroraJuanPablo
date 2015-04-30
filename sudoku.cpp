4ef2cf39eb4a9fea974c05403e1bdb73efe7613c
Pablo Guerra <A01271665@itesm.mx>
2015-04-30 16:34:34 -0500

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,11 +3,11 @@
 //
 // Sudoku
 
-// Aurora Alvarado González
-// A01630329
+// Student Mitzi Sarahi Hernández Jauregui
+// Student A01630243
 // and
-// Juan Pablo Santana González
-// A01229382
+// Student Pablo Daniel Guerra Cervantes
+// Student A01271665
 
 // Standard libraries
 #include <string>
@@ -53,7 +53,8 @@ void populateBoardFromFile(vector< vector<int> >& board, string filename){
       }
     }
   } else {
-    cout << "Something is wrong with the input file" << endl;
+    cout << "\x1b[34m\x1b[40mSomething is wrong with the input file" << endl;
+    cout << "\x1b[39;49m";
   }
 }
 
@@ -63,94 +64,94 @@ void populateBoardFromFile(vector< vector<int> >& board, string filename){
  * Each cell is separated by a space
  * NO SPACE at end of row.
  */
-void printBoard(vector< vector<int> >& board, vector< vector<int> >& Original){
-  // nothing for now, you will implement this for Partial 2 project delivery
-  for(int r = 0; r < BOARDSIZE ; r++){
-    for(int c = 0; c < BOARDSIZE ; c++){
-      if(c > 0){
-  cout << " ";
-      }
+ void printBoard(vector< vector<int> >& board){
+   // nothing for now, you will implement this for Partial 2 project delivery
+   cout << "=====================" << endl;
+   for(int r = 0; r < BOARDSIZE ; r++){
+     cout << "|";
+     for(int c = 0; c < BOARDSIZE ; c++){
+         if(c > 0){
 
-      if (Original[r][c] != 0){
-        // rojo 31
-        cout << "\x1b[31m";
-      } else {
-        if (board[r][c] == 0){
-           // white
-           cout << "\x1b[39m";
-        } else {
-          // verde 41
-          cout << "\x1b[32m";
-        }
-       
-      }
-      cout << board[r][c];
-      // reset 91
-    cout <<  "\x1b[39;49m";
-    }
-    cout << endl;
-  }
+   cout << " ";
+       }
+       cout << board[r][c];
+       if(c%3 == 2){
+         cout << "|";
+       }
+     }
+     cout << endl;
+     if(r%3 == 2){
+       cout << "=====================" << endl;
+     }
+   }
+ }
+
+bool n_check (int row, int column)
+{
+  if ((row < BOARDSIZE) && (column < BOARDSIZE))
+    return true;
+  else
+    cout << "\x1b[34m\x1b[40mPlease enter a number lower than 9, thank you" << endl;
+    return false;
+    cout << "\x1b[39;49m";
 }
 
-bool winning(vector< vector<int> >& board){
-  for(int r = 0; r < BOARDSIZE ; r++){
-    for(int c = 0; c < BOARDSIZE ; c++){
-      if(board [r][c]==0){
+bool n_checkl (int row, int column,int number,vector< vector<int> >& theBoard)
+{
+    for (int i=0; i< BOARDSIZE; i++)
+    {
+      if (theBoard[row][i] == number){
+        cout << "\x1b[34m\x1b[40mInvalid move, an equal numver it´s already in the row" << endl;
         return false;
+        cout << "\x1b[39;49m";
       }
     }
-  }
-  return true;
-}
 
-bool check_rc(int value){
-    if (value >= 0 && value<BOARDSIZE){
-      return true;
-    } else {
-      cout << "Your answer is not valid. Please try again." << endl;
-      return false;
-    }
-}
-
-bool check_num(int value){
-    if (value >= 1 && value<=BOARDSIZE){
-      return true;
-    } else {
-      cout << "Your answer is not valid. Please try again." << endl;
-      return false;
-    }
-}
-
-bool evaluate_rc(int n, int r, int c, vector< vector<int> >& theBoard){
-  for (int i=0; i<9; i++){
-    if (theBoard[r][i]==n){
-      cout << "Invalid movement. The number is repeated in that row." << endl;
-      return false;
-    }
-  }
-  for (int i=0; i<9; i++){
-    if (theBoard[i][c]==n){
-      cout << "Invalid movement. The number is repeated in that column." << endl;
-      return false;
-    }
-  }
-  return true;
-}
-
-bool evaluate_q(int n, int r, int c, vector< vector<int> >& theBoard){
-  int kc, kr, ir, ic;
-  ir = (r/3)*3;
-  ic = (c/3)*3;
-  for(kc=ic; kc < ic +3; kc++){
-    for(kr=ir; kr < ir +3; kr++){
-      if (theBoard [kr][kc]==n){
-        cout << "Invalid movement. The number is repeated in that quadrant."  << endl;
+    for (int i=0; i< BOARDSIZE; i++)
+    {
+      if (theBoard[i][column] == number){
+        cout << "\x1b[34m\x1b[40mInvalid move, an equal number it´s already in the column" << endl;
         return false;
+        cout << "\x1b[39;49m";
+      }
+    }
+    return true;
+}
+bool check (int row, int column)
+{
+  if ((row >= 0 ) && (row<BOARDSIZE) &&(column >= 0) && (column< BOARDSIZE))
+    return true;
+  else
+  {
+  cout <<"\x1b[31m\x1b[40mMust be a number between 0 and 8" << endl;
+  return false;
+  cout << "\x1b[39;49m";
+  }
+}
+
+bool quadrant (int row, int number, int column, vector< vector<int> >& theBoard )
+{
+  int cor, coc,limitr,limitc;
+              // (4,5)
+  cor = (row/3)*3;
+  coc = (column/3)*3;
+
+             //  (1,1) Posición izquierda superior del cuadrante donde se encuentra el número;
+  for (int i=coc; i<coc+3; i++)
+  {
+    for (int j=cor; j<cor+3;j++)
+    {
+      if (theBoard [j][i] == number )
+      {
+        cout << "\x1b[34m\x1b[40mInvalid move, equal number in the same quadrant" << endl;
+      return false;
+      cout << "\x1b[39;49m";
       }
     }
   }
   return true;
 }
+
 /*
  * Your main program goes here.
  * first get the parameters, check if parameter size is 2
@@ -159,101 +160,127 @@ bool evaluate_q(int n, int r, int c, vector< vector<int> >& theBoard){
  * any other number of parameters is illegal
  */
 int main(int argc, char* argv[]) {
+  int column, row,number;
   string filename;
 
   vector< vector<int> > theBoard(BOARDSIZE,vector<int>(BOARDSIZE));
-  vector< vector<int> > Original(BOARDSIZE,vector<int>(BOARDSIZE));
+  vector< vector<int> > programer_Board(BOARDSIZE,vector<int>(BOARDSIZE));
 
   switch(argc)
     {
     case 1:
-      cout << "Please provide the parameters to the program, you must enter a filename to initiate the Sudoku." << endl;
+    cout<< "\x1b[31m **Please provide the parameters to the program, you must enter a filename to initiate the Sudoku.\x1b[39;49m" << endl;
       exit(1);
     case 2:
       filename = argv[1];
       break;
     default:
-      cout << "Please provide the parameters to the program, you must enter a filename to initiate the Sudoku." << endl;
+    cout<< "\x1b[31m **Please provide the parameters to the program, you must enter a filename to initiate the Sudoku.\x1b[39;49m" << endl;
       exit(1);
     }
 
   createZeroBoard(theBoard);
   populateBoardFromFile(theBoard,filename);
-  cout << "Welcome to the Game of Sudoku" << endl;
-  // Here you provide a menu to the user to do what they want
-  // The options are:
-  createZeroBoard(Original);
-  populateBoardFromFile(Original,filename);
 
-  //    print (call the printBoard function)
-  //    write (ask user what number they want to add and where), must accept only legal values
-  //    erase (ask user which position they want to erase), must not erase original values
-  //    quit (quit the program)
+  createZeroBoard(programer_Board);
+  populateBoardFromFile(programer_Board,filename);
+
+
+  cout << "\x1b[37m\x1b[44mWelcome to Sudoku" << endl;
+  cout << endl;
+  cout << endl;
+  cout << "Welcome to the sudoku world, please enjoy" << endl;
+  cout << "The options that are able for you while you play are: "<< endl;
+  cout << "Print: Will print you the board." << endl;
+  cout << "Write: Will ask you for a row and a colum for the new number." << endl;
+  cout << "Quit: Will close the sudoku." << endl;
+  cout << endl;
+  cout << "\x1b[39;49m";
   string userChoice = "";
-  while (winning(theBoard)==false && userChoice != "quit"){
-    printBoard(theBoard,Original);
-    cout << "What would you like to do (write, erase, reset, quit, history): ";
+  cout << "\x1b[35m\x1b[40mLet begin" << endl;
+  do{
+    cout << "\x1b[35m\x1b[40mWhat would you like to do (print, write, erase, quit):" << endl;
     cin >> userChoice;
-    if(userChoice == "history"){
-      cout << endl << "Sudoku (数独 sūdoku?, Digit-single), originally called Number Place,is a logic-based, combinatorial number-placement puzzle. The objective is to fill a 9×9 grid with digits so that each column, each row, and each of the nine 3×3 sub-grids that compose the grid (also called 'boxes', 'blocks', 'regions', or 'sub-squares') contains all of the digits from 1 to 9. The puzzle setter provides a partially completed grid, which for a well-posed puzzle has a unique solution." << endl << endl;
-      continue;
-    }
-    if(userChoice == "reset"){
-      populateBoardFromFile(theBoard,filename);
+    if(userChoice == "print"){
+      cout << "\x1b[33m\x1b[40mThe table is: " << endl;
+      printBoard(theBoard);
       continue;
     }
     if(userChoice == "write"){
-      int r,c,n;
-      do{
-        do{
-          cout << "Which row?" << endl;
-          cin >> r;
-        } while (check_rc(r)!= true);
-        do{
-          cout << "Wich column?"<< endl;
-          cin >> c;
-        } while (check_rc(c)!= true);
-        if (theBoard[r][c] != 0){
-          cout << "You already have a number there." << endl;
-        }
-      } while (theBoard[r][c] != 0);
-      if (Original[r][c]!= 0){
-        cout << "That is an original number and you can't change it" << endl;
-      } else {
-        do {
-          cout << "Which number?" << endl;
-          cin >> n;
-        }while (check_num(n) != true || evaluate_rc (n, r, c, theBoard) !=true || evaluate_q (n, r, c, theBoard) !=true );
-        theBoard[r][c] = n;
-        }
+      do
+      {
+        do
+        {
+          cout << "\x1b[36m\x1b[40mWhere would you like to enter the new number (column) note**(0 to 8) ? " << endl;
+          while (!(cin >> column))
+          {
+            cout<< "must be a number " <<endl;
+            cin.clear();
+            cin.ignore(100, '\n');
+          }
+          cout<< "Where would you like to enter the new number (row) note**(0 to 8) ? " << endl;
+          while (!(cin >> row))
+          {
+            cout<< "must be a number " <<endl;
+            cin.clear();
+            cin.ignore(100, '\n');
+          }
+          if (n_check (column, row) != true)
+          cout<< "\x1b[31m **Invalid move, row and number must be lower than 9\x1b[39;49m" << endl;
+          else
+            if (programer_Board [row][column] != 0)
+            cout<< "\x1b[31m **Original numbers can´t be modified\x1b[39;49m" << endl;
+        }while (check (row,column) != true);
+      }while (n_check (column, row) != true|| programer_Board [row][column] != 0);
+
+      do
+      {
+        cout << "\x1b[39m\x1b[40mGive the new number: "<< endl;
+        cin >> number;
+      }while ((n_checkl(row,column,number,theBoard) != true)  || (quadrant (row,number,column, theBoard) != true));
+      theBoard [row][column] = number;
+      cout << "\x1b[33m\x1b[40mThe table is: " << endl;
+      printBoard(theBoard);
+      cout << "\x1b[39;49m";
       continue;
     }
     if(userChoice == "erase"){
-      int r, c;
-      do{
-        cout << "Which row?" << endl;
-        cin >> r;
-      } while (check_rc(r)!= true);
-      do{
-        cout << "Wich column?"<< endl;
-        cin >> c;
-      } while (check_rc(c)!= true);
-      if (Original[r][c] != 0){
-        cout << "That is an original number and you can't erase it" << endl;
-      } else {
-        theBoard [r][c] = 0;
-      }
+      do {
+        cout << "\x1b[32m\x1b[40mWhere is the  column of the number that has to be erased ? note**(0 to 8)" << endl;
+        while (!(cin >> column)){
+          cout<< "must be a number " <<endl;
+          cin.clear();
+          cin.ignore(100, '\n');
+        }
+        cout<< "Where is the row of the number that has to be erased ? note**(0 to 8)" << endl;
+        while (!(cin >> row)){
+          cout<< "must be a number " <<endl;
+          cin.clear();
+          cin.ignore(100, '\n');
+        }
+        if (n_check (column, row) != true)
+        cout<< "\x1b[31m **Invalid move, row and number must be lower than 9\x1b[39;49m" << endl;
+      }while (n_check (column, row) != true);
+
+      if (programer_Board [row][column] != 0)
+      cout<< "\x1b[31m **Original numbers can´t be modified\x1b[39;49m" << endl;
+        else
+        {
+          theBoard [row][column] = 0;
+          cout << "\x1b[39;49m";
+        }
+      cout << "\x1b[33m\x1b[40mThe table is: " << endl;
+      printBoard(theBoard);
+      cout << "\x1b[39;49m";
       continue;
     }
     if(userChoice == "quit"){
-      cout << "Thanks for playing our game, have a great day!" << endl;
+      cout << "\x1b[34m\x1b[40mThanks for playing our game, have a great day!" << endl;
       continue;
     }
-    cout << "That was not a valid choice, try again." << endl;
-  }
-  if (winning(theBoard)==true){
-  printBoard(theBoard,Original);
-  cout << "Congratulations! You have complete the Sudoku!!" << endl;
-}
+    cout<< "\x1b[31m **That was not a valid choice, try again.\x1b[39;49m" << endl;
+  } while (userChoice != "quit");
+  cout << "\x1b[39;49m";
+
   return 0;
 }
